95a457544af9828969eabff8c28e47bac360e487
chaewonkong <chaewonkong@nexon.co.kr>
2023-08-23 18:54:59 +0900

    feat: add form input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
+<script lang="ts">
+	let visitor = '';
+	let content = '';
+	function handleSubmit(e: SubmitEvent) {
+		if (visitor.length && content.length) {
+			alert(`${visitor}: ${content}`);
+		}
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<form on:submit|preventDefault={handleSubmit}>
+		<div style:display="flex" style:flex-direction="column" style:max-width="600px">
+			<input placeholder="작성자명" bind:value={visitor} />
+			<textarea placeholder="방명록을 작성하세요" bind:value={content} />
+			<button>submit</button>
+		</div>
+	</form>
+</div>
