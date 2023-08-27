ec1b0d2925f3291e133be4b96c373a92ebbffa62
chaewonkong <chaewonkong@gmail.com>
2023-08-27 18:21:39 +0900

    feat: 🎸 add post component

---
+++ src/lib/post.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+export let visitor: string
+export let content: string
+</script>
+
+<div>
+<p>{content}</p>
+<h3>{visitor}</h3>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
+	import Post from "$lib/post.svelte";
+import { get, writable } from "svelte/store";
+
+	interface Post {
+		visitor: string;
+		content: string;
+	}
+
 	let visitor = '';
 	let content = '';
+	let posts: Post[] = []
+
 	function handleSubmit(e: SubmitEvent) {
 		if (visitor.length && content.length) {
-			alert(`${visitor}: ${content}`);
+			posts = [...posts, {visitor, content}]
 		}
+		visitor = ""
+		content = ""
 	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Guest Book</h1>
 <div>
 	<form on:submit|preventDefault={handleSubmit}>
 		<div style:display="flex" style:flex-direction="column" style:max-width="600px">
@@ -19,3 +30,6 @@
 		</div>
 	</form>
 </div>
+{#each posts as post}
+<Post visitor={post.visitor} content={post.content} />
+{/each}
